cb19b81f60df7f79a002481bc3a24e812457e6cd
Wesley Crosslin <136374861+Weskc12@users.noreply.github.com>
2023-08-06 19:00:03 +0000

    javaexcusegen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excusegenerator() {
+    const excuseIntro = [
+      "I didn't turn in my homework because",
+      "I was late for school because",
+      "I didn't show up because",
+    ];
+
+    let atFault = [
+      "my grandma",
+      "my dog",
+      "my brother",
+      "my sister",
+      "my friends",
+    ];
+
+    let reason = [
+      "was late to picking me up",
+      "ate my homework",
+      "ripped it up",
+      "drew all over it",
+      "came over to hangout",
+    ];
+    let excuseIntrogen =
+      excuseIntro[Math.floor(Math.random() * excuseIntro.length)];
+    let atFaultgen = atFault[Math.floor(Math.random() * atFault.length)];
+    let reasonGen = reason[Math.floor(Math.random() * reason.length)];
+    return excuseIntrogen + " " + atFaultgen + " " + reasonGen;
+  }
+  document.querySelector("#excuseGen").innerHTML = excusegenerator();
 };
